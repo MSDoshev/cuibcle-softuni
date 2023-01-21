a1c53643a0616024da7b19190bd6a35cb2b2055c
MSDoshev <mihaildo@outlook.com>
2023-01-21 11:47:03 +0200

    Add search

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 
 const { getCreateCube, postCreateCube, getDetails } = require("./controllers/cubeController");
-const { getHomePage, getAboutPage } = require("./controllers/homeController");
+const { getHomePage, getAboutPage, getErrorPage } = require("./controllers/homeController");
 
 router.get("/", getHomePage);
 
 router.get("/about", getAboutPage);
+router.get("/404", getErrorPage);
 
 router.get("/create", getCreateCube);
 router.post("/create", postCreateCube);

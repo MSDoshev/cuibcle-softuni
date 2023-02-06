c5aebfefb4c5c90c095359927ecbf0e55e1d4693
MSDoshev <mihaildo@outlook.com>
2023-02-06 15:05:37 +0200

    Add new Cube model

--- src/index.js
+++ src/index.js
@@ -16,4 +16,4 @@ app.use(routes);
 initDatabase()
 .then(() => app.listen(config.PORT, () =>
 console.log(`Server is running on port ${config.PORT}...`)))
-.catch((err) => console.log(err));
+.catch((err) => console.log(err.message));

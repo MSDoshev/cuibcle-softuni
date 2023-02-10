08f323d67b670855daa94452c8049b35ed86a9f3
MSDoshev <mihaildo@outlook.com>
2023-02-10 12:52:55 +0200

    Check user and pass on login

--- src/models/User.js
+++ src/models/User.js
@@ -14,13 +14,19 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.pre('save', function(){
-     brcypt.hash(this.password, 10)
+userSchema.pre('save', function(next){
+  brcypt.hash(this.password, 10)
      .then (hash => {
         this.password = hash;
         next();
      });
+    // return isSaved;
 });
+
+userSchema.method('validatePassword', function(password){
+    return brcypt.compare(password, this.password);
+})
+
 const User = mongoose.model('User', userSchema);
 
 module.exports= User;
